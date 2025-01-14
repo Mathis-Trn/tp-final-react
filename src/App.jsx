4f4c3195312c643b49d32bbc05a3810d8a705342
Mathis Ternon <mathis.ternon@devinfluence.fr>
2025-01-14 21:26:16 +0100

    Feat: Ajout du Rooter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router";
 import MovieList from './component/pages/MovieList.jsx'
+import { Link } from "react-router";
 
 function App() {
 
   return (
     <>
-      <MovieList />
+      <nav>
+        <Link to="/">Accueil</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+      </Routes>
     </>
   );
 }
