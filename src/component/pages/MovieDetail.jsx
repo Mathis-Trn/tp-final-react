8b905177af727438623c90b049dda2d6d0bddd11
Mathis Ternon <mathis.ternon@devinfluence.fr>
2025-01-15 09:48:25 +0100

    Feat: Modification du style

--- src/component/pages/MovieDetail.jsx
+++ src/component/pages/MovieDetail.jsx
@@ -31,6 +31,7 @@ const MovieDetail = () => {
             .then(response => response.json())
             .then(data => {
                 setMovie(data);
+                console.log(data);
             })
             .catch(error => {
                 console.error("Erreur dans la recherche de film :", error);
@@ -57,31 +58,43 @@ const MovieDetail = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>{movie.title}</h1>
-            <img className={styles.poster} src={`https://media.themoviedb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-            <p className={styles.overview}>{movie.overview}</p>
-            <div className={styles.details}>
-                <p><strong>Date de sortie :</strong> {movie.release_date}</p>
-                <p><strong>Notation :</strong> {movie.vote_average}</p>
-                <p><strong>Langue d'origine :</strong> {movie.original_language}</p>
-                <p><strong>Nombre de vote :</strong> {movie.vote_count}</p>
-                <p><strong>Durée :</strong> {movie.runtime} minutes</p>
-                <p><strong>Budget :</strong> ${movie.budget.toLocaleString()}</p>
-                <p><strong>Revenu :</strong> ${movie.revenue.toLocaleString()}</p>
-                <p><strong>Statut :</strong> {movie.status}</p>
-                <p><strong>Tagline :</strong> {movie.tagline}</p>
-                <p><strong>Compagnies de production :</strong> {movie.production_companies.map(company => company.name).join(', ')}</p>
-                <p><strong>Pays de production :</strong> {movie.production_countries.map(country => country.name).join(', ')}</p>
-                <p><strong>Langues parlées :</strong> {movie.spoken_languages.map(language => language.name).join(', ')}</p>
-                {movie.belongs_to_collection && (
-                    <div className={styles.collection}>
-                        <h2>Collection : {movie.belongs_to_collection.name}</h2>
-                        <img src={`https://media.themoviedb.org/t/p/w500/${movie.belongs_to_collection.poster_path}`} alt={movie.belongs_to_collection.name} />
+            <div className={styles.contentWrapper}>
+                <div className={styles.posterContainer}>
+                    <img className={styles.poster} src={`https://media.themoviedb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                    <button className={styles.addButton} onClick={() => handleAddMovie(movie)}>
+                        Ajouter à ma Watchlist
+                    </button>
+                </div>
+                <div className={styles.detailsContainer}>
+                    <p className={styles.overview}>{movie.overview}</p>
+                    <div className={styles.details}>
+                        <p><strong>Date de sortie :</strong> {movie.release_date}</p>
+                        <p><strong>Notation :</strong> {movie.vote_average}</p>
+                        <p><strong>Langue d'origine :</strong> {movie.original_language}</p>
+                        <p><strong>Nombre de vote :</strong> {movie.vote_count}</p>
+                        <p><strong>Durée :</strong> {movie.runtime} minutes</p>
+                        <p><strong>Budget :</strong> ${movie.budget.toLocaleString()}</p>
+                        <p><strong>Revenu :</strong> ${movie.revenue.toLocaleString()}</p>
+                        <p><strong>Statut :</strong> {movie.status}</p>
+                        <p><strong>Tagline :</strong> {movie.tagline}</p>
+                        <p><strong>Compagnies de production :</strong> {movie.production_companies.map(company => company.name).join(', ')}</p>
+                        <p><strong>Pays de production :</strong> {movie.production_countries.map(country => country.name).join(', ')}</p>
+                        <p><strong>Langues parlées :</strong> {movie.spoken_languages.map(language => language.name).join(', ')}</p>
                     </div>
-                )}
+                </div>
             </div>
+
+            {movie.belongs_to_collection && (
+                            <div className={styles.collection}>
+                                <h2>Collection : {movie.belongs_to_collection.name}</h2>
+                                <img src={`https://media.themoviedb.org/t/p/w500/${movie.belongs_to_collection.poster_path}`} alt={movie.belongs_to_collection.name} />
+                            </div>
+                        )}
+
             <button className={styles.addButton} onClick={() => handleAddMovie(movie)}>
                 Ajouter à ma Watchlist
             </button>
+
             {showPopup && (
                 <div className={styles.popup}>
                     Film ajouté à votre watchlist!
